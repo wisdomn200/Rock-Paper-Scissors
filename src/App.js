1b409df9c8c229df492a8e2ee27c710222f920ce
wisdomn200 <wisdomn200@gmail.com>
2023-04-24 21:58:43 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Play from "./components/Play";
+import Footer from "./components/Footer";
+import './css/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [myPick, setMyPick] = useState("");
+    const [housePick, setHousePick] = useState("");
+    const [gameScore, setGameScore] = useState(0);
+
+    function newHousePick() {
+        const choices = ["rock", "paper", "scissors"];
+        const randomChoice = choices[Math.floor((Math.random()*3))];
+        setHousePick(randomChoice);
+    }
+
+    useEffect(() => {
+        newHousePick();
+    },[setMyPick]);
+
+    return (
+        <div className="wrapper">
+            <Router>
+                <Header score={gameScore}/>
+                <Route exact path="/">
+                    <Home setPick={setMyPick} />
+                </Route>
+                <Route exact path="/play">
+                    <Play mine={myPick} house={housePick} score={gameScore} setScore={setGameScore} setHousePick={newHousePick}/>
+                </Route>
+                <Footer />
+            </Router>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+import imageRules from '../images/image-rules.svg';
+import closeIcon from "../images/x.svg"
+
+function Footer() {
+
+    const [showModal, setShowModal] = useState(false);
+
+    function toggleModal() {
+        setShowModal(!showModal);
+    }
+
+    return (
+        <div>
+            { showModal? (
+                <Modal>
+                    <div className="modal__header">
+                        <h1>Rules</h1>
+                        <button className="desktop__button" onClick={toggleModal}>
+                            <img src={closeIcon} alt="Close Icon" />
+                        </button>
+                    </div>
+                    <img src={imageRules} alt="Game Rules" />
+                    <button className="mobile__button" onClick={toggleModal}>
+                            <img src={closeIcon} alt="Close Icon" />
+                    </button>
+                </Modal>
+            ) : null }
+        </div>
+    )
+}
+
+export default Footer;
